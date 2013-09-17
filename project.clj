edd1391f20a4dc8fcac8880bfcca4c4112693c9b
Manish Handa <hmanish79@gmail.com>
2013-09-17 15:27:44 -0400

    updated versions

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(def camel-version "2.9.2")
-
-(defproject clj-camel "1.0.0-RELEASE"
+(defproject clj-camel "1.0.1-SNAPSHOT"
   :description "Clojure library wrapping the Apache Camel Java DSL"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.apache.camel/camel-core ~camel-version]
-                 [org.clojure/tools.logging "0.2.3"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.apache.camel/camel-core "2.11.1"]
+                 [org.clojure/tools.logging "0.2.6"]])
